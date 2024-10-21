9b968c9a4a8bd8b9190bd8d37ca37eeb707e0b3d
mfnalex <mfnalex@jeff-media.com>
2024-10-21 17:06:43 +0200

    half-assed working for 1.21

--- test-plugin/src/main/java/asd/MyView.java
+++ test-plugin/src/main/java/asd/MyView.java
@@ -5,14 +5,32 @@ import me.devnatan.inventoryframework.AnvilInput;
 import me.devnatan.inventoryframework.View;
 import me.devnatan.inventoryframework.ViewConfigBuilder;
 import me.devnatan.inventoryframework.ViewType;
+import me.devnatan.inventoryframework.context.RenderContext;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.logging.Logger;
+
 public class MyView extends View {
+
+	private final Logger log = Logger.getLogger(MyView.class.getName());
 
 	private final AnvilInput input = AnvilInput.createAnvilInput();
 
 	@Override
 	public void onInit(@NotNull ViewConfigBuilder config) {
 		config.type(ViewType.ANVIL).with(input);
+		config.title("Hello World!");
+	}
+
+
+	@Override
+	public void onFirstRender(@NotNull RenderContext render) {
+		render.firstSlot(new ItemStack(Material.STONE));
+		render.resultSlot()
+			.onClick(context -> {
+				log.warning(input.get(context));
+			});
 	}
 }
